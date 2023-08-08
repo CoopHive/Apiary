196087be09535f61cb4c62f8c3704656a9721fc1
Levi <lyr262@gmail.com>
2023-08-08 15:59:15 +0200

    address updates and fleshing out simulation plan

--- main.py
+++ main.py
@@ -5,50 +5,93 @@ Test entrypoint script.
 from simulator.contract import Contract, ServiceType, Tx
 import pprint
 
+class Address:
+    def __init__(self):
+        self.current_address = 0
+
+    def get_current_address(self):
+        return self.current_address
+
+    def increment_current_address(self):
+        self.current_address += 1
+
 def main():
     contract = Contract()
 
-    rp_addr_1 = "0x00001"
-    rp_addr_2 = "0x00002"
-    jc_addr_1 = "0x00011"
+    address = Address()
 
-    contract.register_service_provider(
-        ServiceType.SOLVER,
-        "http://foo1.com",
-        {"labels": ["local-test"]},
-        Tx(rp_addr_1, 0)
-    )
+    def create_new_address():
+        if len(contract.wallets) != 0:
+            Address.increment_current_address(address)
+        return hex(Address.get_current_address(address))
 
-    contract.register_service_provider(
-        ServiceType.DIRECTORY,
-        "http://foo2.com",
-        {"labels": ["local-test"]},
-        Tx(rp_addr_2, 0)
-    )
 
-    contract.register_service_provider(
-        ServiceType.JOB_CREATOR,
-        "",
-        {"labels": ["local-test"]},
-        Tx(jc_addr_1, 0)
-    )
 
-    contract.register_service_provider(
-        ServiceType.RESOURCE_PROVIDER,
-        "",
-        {"labels": ["local-test"]},
-        Tx(jc_addr_1, 0)
-    )
+    for _ in range(2):
+        contract.register_service_provider(
+            ServiceType.SOLVER,
+            "http://foo1.com",
+            {"labels": ["local-test"]},
+            Tx(create_new_address(), 0)
+        )
 
+        contract.register_service_provider(
+            ServiceType.DIRECTORY,
+            "http://foo2.com",
+            {"labels": ["local-test"]},
+            Tx(create_new_address(), 0)
+        )
+
+        contract.register_service_provider(
+            ServiceType.JOB_CREATOR,
+            "",
+            {"labels": ["local-test"]},
+            Tx(create_new_address(), 0)
+        )
+
+        contract.register_service_provider(
+            ServiceType.RESOURCE_PROVIDER,
+            "",
+            {"labels": ["local-test"]},
+            Tx(create_new_address(), 0)
+        )
+
+        contract.register_service_provider(
+            ServiceType.MEDIATOR,
+            "",
+            {"labels": ["local-test"]},
+            Tx(create_new_address(), 0)
+        )
+
+    def get_list_of_service_providers(service_type: ServiceType):
+        match service_type:
+            case ServiceType.RESOURCE_PROVIDER:
+                return contract.resource_providers
+            case ServiceType.JOB_CREATOR:
+                return contract.job_creators
+            case ServiceType.SOLVER:
+                return contract.solvers
+            case ServiceType.MEDIATOR:
+                return contract.mediators
+            case ServiceType.DIRECTORY:
+                return contract.directories
 
     print("--> block_number")
     pprint.pprint(contract.block_number) 
     print("--> wallets")
-    pprint.pprint(contract.wallets) 
+    pprint.pprint(contract.wallets)
+
     print("--> resource_providers")
-    pprint.pprint(contract.resource_providers) 
+    pprint.pprint(get_list_of_service_providers(ServiceType.RESOURCE_PROVIDER))
     print("--> job_creators")
-    pprint.pprint(contract.job_creators) 
+    pprint.pprint(get_list_of_service_providers(ServiceType.JOB_CREATOR))
+    print("--> solvers")
+    pprint.pprint(get_list_of_service_providers(ServiceType.SOLVER))
+    print("--> directories")
+    pprint.pprint(get_list_of_service_providers(ServiceType.DIRECTORY))
+    print("--> mediators")
+    pprint.pprint(get_list_of_service_providers(ServiceType.MEDIATOR))
+
 
 
 
--- simulations.md
+++ simulations.md
@@ -2,26 +2,40 @@
 
 A list of the simulations we need to run
 
- * RP = resource provider
- * JC = job creator
- * RO = resource offer
- * JO = job offer
+
+## registrations of service providers
+
+ * register resource provider
+ * register job creator
+ * register solver
+ * register directory 
+ * register mediator
+
 
 ## boot
 
-Register solver and then load solver address into RP & JC
-
- * register solver
  * boot as RP - load solver address from smart contract
  * boot at JC - load solver address from smart contract
+
+
+## registrations of job and resource offers
+
+ * RP registers resource offer to smart contract
+ * JC registers job offer to smart contract
+
+
+## matching
+
+* solver matches job offer and resource offer
+* solver creates deal CID from match
+* solver indicates the match to the two parties
+* (future) parties agree and sign transactions
 
 ## sanity
 
 Just checks the simulator is up and functional:
 
- * same steps as `boot` simulation
- * post RO
- * post JO
- * solver matches
- * RP/JC hear match
- * RP/JC agree match
+ * check for overlapping addresses
+ * check that amount of currency in circulation is constant (accounting for new amounts being added)
+ * JC and RP agree to the same deal CID
+ 
