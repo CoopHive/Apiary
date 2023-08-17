0726b3ac7fb0851b321fca2e2dea0512867cfeea
levirybalov <lyr262@gmail.com>
2023-08-17 12:59:26 +0300

    made events general

--- simulator/solver.py
+++ simulator/solver.py
@@ -9,15 +9,9 @@ class Solver(ServiceProvider):
     def __init__(self, address: str, url: str):
         super().__init__(address, url)
         self.machine_keys = ['CPU', 'RAM']
-        self.match_events = []
+        self.events = []
 
-    # TODO: implement solve()
     def solve(self):
-        """
-        loop over job offers and call match_job_offer
-
-        :return:
-        """
         for job_offer_id, job_offer in self.get_local_information().get_job_offers().items():
             resulting_resource_offer = self.match_job_offer(job_offer)
             if resulting_resource_offer is not None:
@@ -25,19 +19,14 @@ class Solver(ServiceProvider):
                 match.set_id()
                 self.emit_event(match)
 
-
     def match_job_offer(self, job_offer: JobOffer):
         # only look for exact matches for now
-        job_offer_id = job_offer.get_id()
         job_offer_data = job_offer.get_job_offer_data()
-        # job_offer_data_items = job_offer_data.items()
         current_resource_offers = self.local_information.get_resource_offers()
         for resource_offer_id, resource_offer in current_resource_offers.items():
             resource_offer_data = resource_offer.get_resource_offer_data()
-            # resource_offer_data_items = resource_offer_data.items()
             is_match = True
             for machine_key in self.machine_keys:
-                # if resource_offer_data_items[machine_key] != job_offer_data_items[machine_key]:
                 if resource_offer_data[machine_key] != job_offer_data[machine_key]:
                     is_match = False
 
@@ -59,10 +48,10 @@ class Solver(ServiceProvider):
         return match
 
     def get_events(self):
-        return self.match_events
+        return self.events
 
     def emit_event(self, match: Match):
-        self.match_events.append(match)
+        self.events.append(match)
 
     # TODO: change to subscribe_event()
     # def subscribe_deal(self, handler: ServiceProvider.handler_filter_by_owner_public_key, deal: Deal):
