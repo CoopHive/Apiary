27daec0d8a08321379f43d7ff57b40ba79b54fac
matteoettam09 <matteomanzi09@gmail.com>
2024-07-10 16:03:17 +0200

    fix: docs

--- coophive_simulator/hash_dict.py
+++ coophive_simulator/hash_dict.py
@@ -3,6 +3,15 @@ import json
 
 
 def hash_dict(dict) -> str:
+    """
+    Generate a SHA-256 hash of a dictionary.
+
+    Args:
+        dict (dict): Dictionary to be hashed.
+
+    Returns:
+        str: Hexadecimal representation of the SHA-256 hash.
+    """
     hash_function = hashlib.sha256()
     encoded = json.dumps(dict, sort_keys=True).encode()
     hash_function.update(encoded)
