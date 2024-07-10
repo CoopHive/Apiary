c6a0e92ae90c313b4532bfd671f17a78bad33215
matteoettam09 <matteomanzi09@gmail.com>
2024-07-10 20:51:54 +0200

    chore: job refacto

--- coophive_simulator/job_offer.py
+++ coophive_simulator/job_offer.py
@@ -1,8 +1,26 @@
+"""Define the JobOffer class which extends DataAttribute."""
+
 from coophive_simulator.data_attribute import DataAttribute
-from coophive_simulator.job_offer_attributes_dict import job_offer_attributes
+
+job_offer_attributes = {
+    "owner",
+    "target_client",
+    "created_at",
+    "timeout",
+    "CPU",
+    "GPU",
+    "RAM",
+    "module",
+    "prices",
+    "verification_method",
+    "mediators",
+}
 
 
 class JobOffer(DataAttribute):
+    """Represents a job-offer object that inherits from DataAttribute."""
+
     def __init__(self):
+        """Initializes a JobOffer object."""
         super().__init__()
         self.data_attributes = job_offer_attributes
