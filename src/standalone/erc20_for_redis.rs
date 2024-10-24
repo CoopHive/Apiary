937048370f401f4bfc1554e51078d1b0e5bf6ce4
疒奀 <46226846+mlegls@users.noreply.github.com>
2024-10-24 16:54:02 +0800

    make buy statement

--- src/standalone/erc20_for_redis.rs
+++ src/standalone/erc20_for_redis.rs
@@ -5,9 +5,7 @@ use alloy::{
 };
 use std::env;
 
-use crate::contracts::{
-    ERC20PaymentObligation, RedisProvisionObligation, TrustedPartyArbiter, IEAS, IERC20,
-};
+use crate::contracts::{ERC20PaymentObligation, RedisProvisionObligation, IEAS, IERC20};
 use crate::provider;
 
 sol! {
@@ -42,9 +40,11 @@ pub async fn make_buy_statement(
 ) -> eyre::Result<FixedBytes<32>> {
     let provider = provider::get_provider(private_key)?;
 
+    let payment_address =
+        env::var("ERC20_PAYMENT_OBLIGATION").map(|a| Address::parse_checksummed(a, None))??;
     let redis_provision_obligation_address =
         env::var("REDIS_PROVISION_OBLIGATION").map(|a| Address::parse_checksummed(a, None))??;
-    let arbiter =
+    let arbiter_address =
         env::var("TRUSTED_PARTY_ARBITER").map(|a| Address::parse_checksummed(a, None))??;
 
     let base_demand: Bytes = demand.abi_encode().into();
@@ -52,7 +52,47 @@ pub async fn make_buy_statement(
         creator: service_provider,
         baseArbiter: redis_provision_obligation_address,
         baseDemand: base_demand,
+    }
+    .abi_encode()
+    .into();
+
+    let token_contract = IERC20::new(price.token, &provider);
+    let statement_contract = ERC20PaymentObligation::new(payment_address, &provider);
+
+    let approval_receipt = token_contract
+        .approve(payment_address, price.amount)
+        .send()
+        .await?
+        .get_receipt()
+        .await?;
+
+    if !approval_receipt.status() {
+        return Err(eyre::eyre!("approval failed"));
     };
 
-    Ok(FixedBytes::<32>::default())
+    let log = statement_contract
+        .makeStatement(
+            ERC20PaymentObligation::StatementData {
+                token: price.token,
+                amount: price.amount,
+                arbiter: arbiter_address,
+                demand,
+            },
+            0,
+            b256!("0000000000000000000000000000000000000000000000000000000000000000"),
+        )
+        .send()
+        .await?
+        .get_receipt()
+        .await?
+        .inner
+        .logs()
+        .iter()
+        .filter(|log| log.topic0() == Some(&IEAS::Attested::SIGNATURE_HASH))
+        .collect::<Vec<_>>()
+        .first()
+        .map(|log| log.log_decode::<IEAS::Attested>())
+        .ok_or_else(|| eyre::eyre!("makeStatement logs didn't contain Attest"))??;
+
+    Ok(log.inner.uid)
 }
