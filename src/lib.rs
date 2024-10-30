ed16c9ab341c79213d5477f9d61f5dc12f018e49
疒奀 <46226846+mlegls@users.noreply.github.com>
2024-10-30 18:28:14 +0800

    clean up shared

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 use pyo3::prelude::*;
 
+pub mod bundle_for_job;
 pub mod contracts;
 pub mod erc20_for_job;
 pub mod erc721_for_job;
-pub mod bundle_for_job;
 
+pub mod apiary;
 pub mod provider;
 pub mod shared;
-pub mod apiary;
 
 /// A Python module implemented in Rust.
 #[pymodule]
@@ -16,6 +16,5 @@ fn apiars(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     erc721_for_job::add_erc721_submodule(py, m)?;
     bundle_for_job::add_bundle_submodule(py, m)?;
 
-    m.add_function(wrap_pyfunction!(shared::get_result_cid_from_sell_uid, m)?)?;
     Ok(())
 }
